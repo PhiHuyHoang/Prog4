ee179bda29432a8d0581ca10609909b64d24b4f1
rice <luumanhlapdi@gmail.com>
2019-02-12 19:08:57 +0100

    two exercise

---
+++ prog4_01_notepad_editor/prog4_01_notepad_editor/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prog4_01_notepad_editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // learn about DOCKPANNEL 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true) // model window
+            {
+                txtContent.Text = File.ReadAllText(ofd.FileName);
+
+            }
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if(sfd.ShowDialog() == true)
+            {
+                File.WriteAllText(sfd.FileName, txtContent.Text);
+                Title = sfd.FileName;
+            }
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var answer = MessageBox.Show("Save file?", "QUESTION", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            if (answer == MessageBoxResult.Yes)
+            {
+                SaveClick(this, null);
+            }
+            else if (answer == MessageBoxResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
